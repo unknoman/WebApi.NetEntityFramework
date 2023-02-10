7a07e6b87f0870b96eb496335ac153b2b8ed5f1d
emanuel oviedo <cjo1@live.com>
2023-02-09 21:25:03 -0300

    Ordenando proyecto

--- Modelos/Modelos/Tipouser.cs
+++ Modelos/Modelos/Tipouser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace WebApplication1.Modelos;
 
@@ -9,5 +10,6 @@ public partial class Tipouser
 
     public string Tipo { get; set; } = null!;
 
+    [JsonIgnore]
     public virtual ICollection<Usuario> Usuarios { get; } = new List<Usuario>();
 }
--- Modelos/Modelos/Usuario.cs
+++ Modelos/Modelos/Usuario.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Castle.Components.DictionaryAdapter;
+using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace WebApplication1.Modelos;
 
@@ -12,7 +15,8 @@ public partial class Usuario
 
     public string Password { get; set; } = null!;
 
-    public virtual Tipouser IdtipoNavigation { get; set; } = null!;
+    [JsonIgnore]
+    public virtual Tipouser? IdtipoNavigation { get; set; } = null!;
 
     public int Idtipo { get; set; }
 
